d1eae9fd8f1bad0a05628d3de62f89b636df4adf
NO GAME NO LIFE <qqlcx5@163.com>
2019-04-14 00:16:42 +0800

    auto

--- push.sh
+++ push.sh
@@ -1,5 +1,4 @@
 git init
 git add .
 git commit -m "autoPush"
-git remote add origin git@github.com:qqlcx5/editor.git
-git push -u -f origin master
+git push -u -f git@github.com:qqlcx5/editor.git webstorm
